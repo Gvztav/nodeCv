aae349b5117589bb88e5cfea35331374196b6bd0
Gvztav <gustavogn2015@yahoo.com.ar>
2016-04-08 16:49:16 -0300

    Update server.js
    otro camios

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express        = require('express');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var app            = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users                   // log every request to the console
 app.use(bodyParser.json())    // parse application/json
 
