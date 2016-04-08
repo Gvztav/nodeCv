e18cf06913af2fcf5f7b82974bdde8218a392fe8
Gvztav <gustavogn2015@yahoo.com.ar>
2016-04-08 16:48:28 -0300

    Update server.js
    puerto

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express        = require('express');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var app            = express();
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users                   // log every request to the console
 app.use(bodyParser.json())    // parse application/json
 
-app.listen(5000);   
-console.log('Magic happens on port 8080');          // shoutout to the use
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
