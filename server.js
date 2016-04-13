360f9723df0add533922d67cece92dcd4bd8adc6
Gustavo Fernando Gennari <ggennari@softtek.com>
2016-04-13 15:59:57 -0300

    modificando puerto a 3000

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var app            = express();
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users                   // log every request to the console
 app.use(bodyParser.json())    // parse application/json
 
-app.listen(8080);   
-console.log('Magic happens on port 8080');          // shoutout to the use
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
