15afe8e3b8d6fdd7305494f83a44ece367ddb057
Gvztav <gustavogn2015@yahoo.com.ar>
2017-03-25 11:09:08 -0300

    mejora de cv

--- public/js/progressBar.js
+++ public/js/progressBar.js
@@ -1,12 +1,12 @@
 
        $(document).ready(function(){
-            progressBar(15, $('.progressBarBootstrap'));
-            progressBar(30, $('.progressBarPLSQL'));
-            progressBar(40, $('.progressBarHTML5CSS'));
-            progressBar(10, $('.progressBarAngular'));
-            progressBar(20, $('.progressBarPHP'));
-            progressBar(50, $('.progressBarJava'));
-            progressBar(30, $('.progressBarJavascriptjQuery'));
+            progressBar(25, $('.progressBarBootstrap'));
+            progressBar(40, $('.progressBarPLSQL'));
+            progressBar(60, $('.progressBarHTML5CSS'));
+            progressBar(20, $('.progressBarAngular'));
+            progressBar(40, $('.progressBarPHP'));
+            progressBar(60, $('.progressBarJava'));
+            progressBar(40, $('.progressBarJavascriptjQuery'));
             
 
                 function progressBar(percent, $element) {
